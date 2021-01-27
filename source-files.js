57aed6bb43327c2499f3200cfe42231e20b9d96c
Holzhaus <Holzhaus@users.noreply.github.com>
2021-01-27 10:28:17 +0000

    deploy: 34219ef201c6975b90d6116fda452b6cf4d7f386

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["serato_tags"] = {"name":"","files":["analysis.rs","autotags.rs","beatgrid.rs","container.rs","lib.rs","markers.rs","markers2.rs","overview.rs","util.rs"]};
+sourcesIndex["serato_tags"] = {"name":"","dirs":[{"name":"analysis","files":["mod.rs"]},{"name":"autotags","files":["mod.rs"]},{"name":"beatgrid","files":["mod.rs"]},{"name":"markers","files":["mod.rs"]},{"name":"markers2","files":["mod.rs"]},{"name":"overview","files":["mod.rs"]}],"files":["container.rs","lib.rs","util.rs"]};
 createSourceSidebar();
