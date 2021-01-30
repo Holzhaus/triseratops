cc85dfecb34890fa7436043edb8438b5ada3717f
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2021-01-30 22:57:25 +0100

    library/filesystem: Implement method to find library path info

--- src/library/filesystem.rs
+++ src/library/filesystem.rs
@@ -1,19 +1,62 @@
 use std::path::PathBuf;
 
-pub fn get_serato_database(dir: &PathBuf) -> Option<PathBuf> {
-    if !dir.is_dir() {
+pub type CrateInfo = (String, PathBuf);
+
+#[derive(Debug, PartialEq)]
+pub struct SeratoLibraryInfo {
+    pub path: PathBuf,
+    pub database_path: PathBuf,
+    pub crates: Vec<CrateInfo>,
+}
+
+pub fn get_library(path: &PathBuf) -> Option<SeratoLibraryInfo> {
+    if !path.is_dir() {
         return None;
     }
 
-    let mut path = dir.join("_Serato_");
-    if !path.exists() {
+    let serato_path = path.join("_Serato_");
+    if !serato_path.exists() {
         return None;
     }
 
-    path.push("database V2");
-    if !path.exists() {
+    let database_path = serato_path.join("database V2");
+    if !database_path.exists() {
         return None;
     }
 
-    Some(path)
+    let crates_path = serato_path.join("Subcrates");
+    let mut crates = vec![];
+    if let Ok(entries) = crates_path.read_dir() {
+        for entry in entries {
+            let entry = match entry.ok() {
+                Some(x) => x,
+                None => {
+                    continue;
+                }
+            };
+
+            let crate_path = entry.path();
+
+            if !crate_path.is_file() {
+                continue;
+            }
+
+            if let Some(ext) = crate_path.extension() {
+                if ext != "crate" {
+                    continue;
+                }
+                if let Some(crate_name_osstr) = crate_path.file_stem() {
+                    if let Some(crate_name) = crate_name_osstr.to_str() {
+                        crates.push((crate_name.to_string(), crate_path));
+                    }
+                }
+            }
+        }
+    }
+
+    Some(SeratoLibraryInfo {
+        path: path.to_path_buf(),
+        database_path,
+        crates,
+    })
 }
--- tests/library.rs
+++ tests/library.rs
@@ -6,9 +6,26 @@ use std::path::PathBuf;
 #[test]
 fn library_dir_detection() {
     let music_dir = PathBuf::from("tests/data/library/usb_drive");
-    let db_path = filesystem::get_serato_database(&music_dir).unwrap();
+    let info = filesystem::get_library(&music_dir).unwrap();
     assert_eq!(
-        db_path,
-        PathBuf::from("tests/data/library/usb_drive/_Serato_/database V2")
+        info,
+        filesystem::SeratoLibraryInfo {
+            path: PathBuf::from("tests/data/library/usb_drive"),
+            database_path: PathBuf::from("tests/data/library/usb_drive/_Serato_/database V2"),
+            crates: vec![
+                (
+                    String::from("80s Mashup"),
+                    PathBuf::from(
+                        "tests/data/library/usb_drive/_Serato_/Subcrates/80s Mashup.crate"
+                    )
+                ),
+                (
+                    String::from("French House"),
+                    PathBuf::from(
+                        "tests/data/library/usb_drive/_Serato_/Subcrates/French House.crate"
+                    )
+                ),
+            ],
+        }
     );
 }
