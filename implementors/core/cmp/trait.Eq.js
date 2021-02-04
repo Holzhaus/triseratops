1ff3fe8ff75dfec7068e87d4b4c80ee88ad9becd
Holzhaus <Holzhaus@users.noreply.github.com>
2021-02-04 14:27:34 +0000

    deploy: 9e45ffc7453932149b9a51bd51f72e62bcb7c06e

--- implementors/core/cmp/trait.Eq.js
+++ implementors/core/cmp/trait.Eq.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["seratodj"] = [{"text":"impl Eq for Version","synthetic":false,"types":[]}];
+implementors["triseratops"] = [{"text":"impl Eq for Version","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/core/marker/trait.Copy.js
+++ implementors/core/marker/trait.Copy.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["seratodj"] = [{"text":"impl Copy for Color","synthetic":false,"types":[]}];
+implementors["triseratops"] = [{"text":"impl Copy for Color","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
