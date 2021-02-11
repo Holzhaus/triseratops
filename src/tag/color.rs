4e13516c8d78383df93209f2c4bcb8d3fac02dc1
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2021-02-11 15:28:34 +0100

    tag/color: Implement converting color from/to u32

--- src/tag/color.rs
+++ src/tag/color.rs
@@ -5,3 +5,18 @@ pub struct Color {
     pub green: u8,
     pub blue: u8,
 }
+
+impl From<u32> for Color {
+    fn from(code: u32) -> Self {
+        let blue = (code & 0xFF) as u8;
+        let green = ((code >> 8) & 0xFF) as u8;
+        let red = ((code >> 16) & 0xFF) as u8;
+        Color { red, green, blue }
+    }
+}
+
+impl Into<u32> for Color {
+    fn into(self) -> u32 {
+        (self.red as u32) << 16 | (self.green as u32) << 8 | self.blue as u32
+    }
+}
